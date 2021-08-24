f898d3e9ac278f4101979180a402e6f652a7b5c3
Kristoffer Kiil <krki@ssi.dk>
2021-08-24 15:13:15 +0200

    Bump version: v1_0_1__ → v1_0_2__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrost_seqsero',
-    version='v1_0_1__',
+    version='v1_0_2__',
     description='SeqSero component for salmonella serotyping',
     url='https://github.com/ssi-dk/bifrost_seqsero',
     author="Kristoffer Kiil",
